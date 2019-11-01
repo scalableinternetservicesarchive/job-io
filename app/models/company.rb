68bcabd18303035a13642e003ac9a08f4fcba0e5
Luca Matsumoto <lucamatsumoto@gmail.com>
2019-11-01 01:49:37 -0700

    add search bar to searching companies and somewhat functional back buttons

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,18 @@
 class Company < ApplicationRecord
     # validate uniqueness and presence when creating a company
     validates :name, presence: true, uniqueness: true
+
+    def self.search(search)
+        if search
+            company = Company.find_by(name: search)
+            if company
+                self.where(id: company)
+            else
+                Company.all
+            end
+        else
+            Company.all
+        end  
+    end
+
 end
