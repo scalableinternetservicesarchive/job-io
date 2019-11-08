7e0ff44f22d017c29ce7ffd2a60f12f388ae90e9
Terrence Ho <terrenceho.books@gmail.com>
2019-11-08 10:38:15 -0800

    Deleted problematic migrations

--- db/migrate/20191108041215_remove_reference_from_company.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReferenceFromCompany < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :companies, :user_id_id
-  end
-end
--- db/migrate/20191108045816_remove_join_table.rb
+++
@@ -1,8 +0,0 @@
-class RemoveJoinTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_join_table :users, :companies do |t|
-      # t.index [:user_id, :company_id]
-      # t.index [:company_id, :user_id]
-    end
-  end
-end
