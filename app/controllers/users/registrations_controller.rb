0d166b228ffb0c4bc421fc7d3129168fef20a729
Terrence Ho <terrenceho.books@gmail.com>
2019-11-08 12:36:53 -0800

    Add user resume to edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,9 +16,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+    @user = User.find(current_user.id)
+  end
 
   # PUT /resource
   # def update
