0b0edfc6b4ff783b08fc8e240c2a34837d931822
Terrence Ho <terrenceho.books@gmail.com>
2019-11-27 13:00:28 -0800

    Remove resume validation and fixed user load test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # attr_accessible :email, :encrypted_password, :username, :first_name, :last_name, :summary, :remember_me, :resume_link, :resume_link_cache, :remove_resume_link
-  validates_presence_of   :resume
-
   has_and_belongs_to_many :companies
 end
